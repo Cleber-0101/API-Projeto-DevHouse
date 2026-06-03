732d8d58371408a06a94d45662bd1f9a75284c77
Cleber-0101 <cleberbatista99@hotmail.com>
2026-06-02 21:36:47 -0300

    Criando validação ultilizando middewares validações entre cliente e servidor

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const server = express()
 //com isso eu consigo enviar no corpo da requsição e criar um CURSO passando os dados via corpo da requisição
 server.use(express.json());
 
+// poderia ser um banco de dados tambem
 const Bancocurso = ["Node.js", "TypeScript", "SqlServer", "SpringBoot", "JavaScript avançado"]
 
-//Criado middlewares
+//Criando middlewares
 server.use((req, res, next) => {
-    console.log('REQUISIÇÃO CHAMADA')
+    console.log(`URL CHAMADA  ${req.url}`)
 
     return next();
 })
@@ -22,20 +23,33 @@ function chackandoDados(req, res, next) {
     return next();
 }
 
+//criando um middare que verificar o tamanho da string
+function validandoTamanhoDaString(req, res, next) {
+    const { name } = req.body
+
+    // o nome passado precisa ser maior que 3 caracteres 
+    if (!name || name.length < 3) {
+        return res.status(400).json({
+            erro: "O nome precisa ser maior que 3 caracteres"
+        })
+    }
+
+    return next()
+}
 
 //Lista todos os cursos
-server.get('/curso', (req, res) => {
+server.get('/curso', chackandoDados, (req, res) => {
     return res.json(Bancocurso)
 })
 
 //Lista por parametros
 server.get('/cursos/:index', (req, res) => {
-    const { index } = req.params;
+    const { index } = req.params; // requisição via URL por isso é request params 
     return res.json(Bancocurso[index]);
 })
 
 //criando || Ultilizando Middlwares
-server.post('/curso', chackandoDados , (req, res) => {
+server.post('/curso', chackandoDados, validandoTamanhoDaString ,(req, res) => {
     const { name } = req.body
     Bancocurso.push(name)
 
@@ -43,8 +57,8 @@ server.post('/curso', chackandoDados , (req, res) => {
 })
 
 //Atualizando curso 
-server.put('/cursos/:index', chackandoDados,  (req, res) => {
-    const { index } = req.params
+server.put('/cursos/:index', chackandoDados, (req, res) => {
+    const { index } = req.params // requisição via url por isso é requeste params
     const { name } = req.body
 
     Bancocurso[index] = name;
